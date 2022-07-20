8e104c5185ed6f45bfc6925c2232fb89a7288e77
伊東和成 <itokazunari5375@icloud.com>
2022-07-20 22:23:34 +0900

    認証済みユーザーのみユーザー情報を取得できる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def index
-    render json: User.all
+    if current_user
+      render json: User.all
+    else 
+      render json: { is_login: false, message: "ユーザーが存在しません" }
+    end
+
   end
 end
