828019fd35671422f0abd4275250d28f9be7d897
伊東和成 <itokazunari5375@icloud.com>
2022-07-21 10:54:59 +0900

    ユーザーidが1のデータを返す

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def index
     user = User.find(1)
-    following = user.active_relationships
-    render json: following
+    # ユーザーIDが1のフォローリスト
+    active_relationships = user.active_relationships
+    followingLists = active_relationships.map{|i| 
+      User.find(i.followed_id)
+    }
+    render json: followingLists
   end
 end
